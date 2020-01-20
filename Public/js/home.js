940dfbc559093fe0f898cf9fa8dcfd4b4e975134
dineshdsv3 <dineshdsv3@gmail.com>
2020-01-20 11:23:32 +0530

    Created Home Page

---
+++ Public/js/home.js
@@ -0,0 +1,9 @@
+console.log("Welcome to home page");
+
+function  restaurantLogin(){
+    window.location.pathname ="/html/restaurantLogin.html";
+}
+
+function userLogin() {
+    window.location.pathname ="/html/userLogin.html";
+}
